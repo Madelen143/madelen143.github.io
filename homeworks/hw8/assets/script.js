ab8a9ba6a8244c412e70cfcc0d5516e328ecaccf
Madelen <madelenf143@berkeley.edu>
2023-08-26 16:24:57 -0700

    updated hw8

--- homeworks/hw8/assets/script.js
+++ homeworks/hw8/assets/script.js
@@ -2,3 +2,5 @@ function toggleSongs() {
     var secondSongs = document.getElementById("songs-second-half");
     secondSongs.classList.toggle("hidden");
 }
+
+document.getElementById("toggle-button").onclick = toggleSongs;
